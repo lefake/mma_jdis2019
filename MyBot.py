58b4de0b373a2b0f2e86f3b1c502e7750808f2c6
Marc-Antoine Lafreniere <marc4492@gmail.com>
2019-07-13 15:10:50 -0400

    Defense
    Remove power up

--- MyBot.py
+++ MyBot.py
@@ -37,52 +37,66 @@ def createTeam(firstIndex, secondIndex, isRed):
 ##########
 
 class AgentOne(CaptureAgent):
-    """
-    A Dummy agent to serve as an example of the necessary agent structure.
-    You should look at baselineTeam.py for more details about how to
-    create an agent as this is the bare minimum.
-    """
+    def initFoodList(self):
+        tempFood = []
+        numrows = len(list(self.getFood(self.game)))   
+        numcols = len(list(self.getFood(self.game)[0]))
+        
+        self.foodList = []
+        for i in range(numrows):
+            for j in range(numcols):
+                if self.getFood(self.game)[i][j]:
+                    self.foodList.append((i, j))
 
     def registerInitialState(self, gameState: GameState):
-        """
-        This method handles the initial setup of the
-        agent to populate useful fields (such as what team
-        we're on).
-
-        A distanceCalculator instance caches the maze distances
-        between each pair of positions, so your agents can use:
-        self.distancer.getDistance(p1, p2)
-
-        IMPORTANT: This method may run for at most 5 seconds.
-        """
-
-        '''
-        Make sure you do not delete the following line. If you would like to
-        use Manhattan distances instead of maze distances in order to save
-        on initialization time, please take a look at
-        CaptureAgent.registerInitialState in captureAgents.py.
-        '''
         CaptureAgent.registerInitialState(self, gameState)
-
-        '''
-        Your initialization code goes here, if you need any.
-        '''
+        self.game = gameState
+        self.initFoodList()
 
     def chooseAction(self, gameState: GameState) -> str:
-        """
-        Picks among legal actions randomly.
-        """
         actions = gameState.getLegalActions(self.index)
+        if "FREEZE" in actions != -1: actions.remove("FREEZE")
+        if "Stop" in actions: actions.remove("Stop")
+        if "Jump_East" in actions: actions.remove("Jump_East")
+        if "Jump_West" in actions: actions.remove("Jump_West")
+        if "Jump_West" in actions: actions.remove("Jump_West")
+        if "Jump_North" in actions: actions.remove("Jump_North")
+        if "Jump_South" in actions: actions.remove("Jump_South")
+        
+        myState = gameState.getAgentState(self.index)
+        myPos = myState.getPosition()
 
+        closest = self.foodList[0]
+
+        for food in self.foodList:
+            if self.getMazeDistance(myPos, food) < self.getMazeDistance(myPos, closest):
+                closest = food
+
+        
+        
         return random.choice(actions)
 
 
 class AgentTwo(CaptureAgent):
+    def initFoodDefList(self):
+        tempFood = []
+        numrows = len(list(self.getFoodYouAreDefending(self.game)))   
+        numcols = len(list(self.getFoodYouAreDefending(self.game)[0]))
+        
+        self.foodDefList = []
+        for i in range(numrows):
+            for j in range(numcols):
+                if self.getFoodYouAreDefending(self.game)[i][j]:
+                    self.foodDefList.append((i, j))
+
+
     def registerInitialState(self, gameState: GameState):
         CaptureAgent.registerInitialState(self, gameState)
+        self.game = gameState
+        self.initFoodDefList()
     
     def chooseAction(self, gameState: GameState) -> str:
-        capMultiplier = 3
+        capMultiplier = 2
 
         if self.red:
             capsules = gameState.getRedCapsules()
@@ -91,13 +105,20 @@ class AgentTwo(CaptureAgent):
 
         foodList = list(self.getFood(gameState))
         actions = gameState.getLegalActions(self.index)
-                
+        if "FREEZE" in actions != -1: actions.remove("FREEZE")
+        if "Stop" in actions: actions.remove("Stop")
+        if "Jump_East" in actions: actions.remove("Jump_East")
+        if "Jump_West" in actions: actions.remove("Jump_West")
+        if "Jump_West" in actions: actions.remove("Jump_West")
+        if "Jump_North" in actions: actions.remove("Jump_North")
+        if "Jump_South" in actions: actions.remove("Jump_South")
+        
         meanX = 0
         meanY = 0
         
-        for i in range(len(foodList)):
-            meanX = meanX + foodList[i][0]
-            meanY = meanY + foodList[i][1]
+        for i in range(len(self.foodDefList)):
+            meanX = meanX + self.foodDefList[i][0]
+            meanY = meanY + self.foodDefList[i][1]
 
         for cap in capsules:
             meanX = capMultiplier* meanX + cap[0]
@@ -110,22 +131,6 @@ class AgentTwo(CaptureAgent):
         myPos = myState.getPosition()
 
         curDistance = abs(myPos[0] - meanX) + abs(myPos[1] - meanY)
-
-
-        '''if meanX > myPos[0]:
-            if actions.__contains__('East'):
-                return 'East'
-        if meanX <= myPos[0]
-            if actions.__contains__('West'):
-                return 'West'
-        if meanY > myPos[1]:
-            if actions.__contains__('East'):
-                return 'East'
-        if meanY <= myPos[1]
-            if actions.__contains__('West'):
-                return 'West'
-        '''
-
 
         for action in actions:
             gameState.generateSuccessor(self.index, action)
